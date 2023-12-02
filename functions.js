fb5eb17503c47568cb0bcfaa1c2b96ed503151a3
Richard Kuo <rkuo2000@gmail.com>
2023-12-03 03:21:13 +0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,11 +3,11 @@ function calculate() {
   let table = document.getElementById("myTable");
   let tr = table.getElementsByTagName("tr");
   let subTotal = 0;
-  for (var i = 0; i < ele.length; i++) {
-    let td = tr[i].getElementsByTagName("td")[1];
+  for (var i = 0; i < ele.length; i++) {    
+    let td = tr[i].getElementsByTagName("item"+str(i))[1];
     let price = parseFloat(td.innerText); // change here
     if (ele[i].type == 'checkbox' && ele[i].checked == true)
-      subTotal += price;
+      subTotal += price * number;
   }
   document.getElementById("val").innerHTML = "The subtotal is " + subTotal.toFixed(2); // and set precision here
 }
